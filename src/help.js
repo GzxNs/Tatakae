f02c9986b3760bd7aedded358a5da428bc88c85a
Sasukezin <77336228+Sasukezin@users.noreply.github.com>
2021-01-12 10:40:10 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,283 @@
+const help = (prefix) => {
+	return `
+
+
+╭━⋆⃟⊱๑⋆⃟⊱๑ˌ⋆⃟⊱๑⋆⃟⊱๑⋆⃟⊱๑⋆⃟⊱๑⋆⃟⊱๑⋆⃟⊱๑⋆⃟⊱๑⋆⃟⊱๑⋆⃟⊱━
+〲┴┬➣♠️𝐄𝐑𝐄𝐍 𝐁𝐎𝐓♠️
+〲
+〲┴┬➣𝐃𝐎𝐍𝐎: 𝐆𝐀𝐀𝐑𝐀𝐙𝐈𝐍𝐇𝐎 愛
+〲
+〲┴┬➣wa.me/+5596984081026
+〲
+〲┴┬➣𝐒𝐓𝐀𝐓𝐔𝐒: ON
+〲︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶
+〲╭─────⊣〘 𝗙𝗜𝗚𝗨𝗥𝗜𝗡𝗛𝗔 〙
+〲│
+〲┴┬➣ COMANDO: '${prefix}sticker'
+〲┴┬➣ DESC: Converter em figurinha
+〲┴┬➣ USO: Converter em figurinha
+〲
+〲┴┬➣ COMANDO: '${prefix}sticker nobg'
+〲┴┬➣ DESC: Imagem em figurinha sem fundo
+〲┴┬➣ USO: Enviar imagem com legenda
+〲
+〲┴┬➣ COMANDO: '${prefix}toimg'
+〲┴┬➣ DESC: Converter figurinha em imagem
+〲┴┬➣ USO: Figurinha de resposta
+〲
+〲┴┬➣ COMANDO: '${prefix}tsticker'
+〲┴┬➣ DESC: Converter texto em Figurinha
+〲┴┬➣ USO: .tsticker [seu texto]
+〲
+〲︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶
+〲
+〲╭─────⊣〘 𝗠𝗘𝗠𝗘 〙
+〲│
+〲┴┬➣ COMANDO: '${prefix}meme'
+〲┴┬➣ DESC: Imagens aleatórias de meme
+〲┴┬➣ USO: Apenas envie o comando
+〲
+〲┴┬➣ COMANDO: '${prefix}memeindo'
+〲┴┬➣ DESC: Imagens aleatórias de meme
+〲┴┬➣ USO: Apenas envie o comando
+〲
+〲︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶
+〲
+〲╭─────⊣〘 𝗢𝗨𝗧𝗥𝗢𝗦 〙 
+〲│
+〲┴┬➣ COMANDO: '${prefix}gtts'
+〲┴┬➣ DESC: Fazer o bot gravar áudio
+〲┴┬➣ USO: .gtts [cc] [text]
+〲┴┬➣ EXEMPLO: .gtts ja On2-chan
+〲
+〲┴┬➣ COMANDO: '${prefix}url2img'
+〲┴┬➣ DESC: Tirar screenshots da web
+〲┴┬➣ USO: .url2img [tipe] [url]
+〲
+〲┴┬➣ COMANDO: '${prefix}simi'
+〲┴┬➣ DESC: Sua mensagem será respondida
+〲┴┬➣ USO: .simi [sua mensagem]
+〲
+〲┴┬➣ COMANDO: '${prefix}ocr'
+〲┴┬➣ DESC: Pegue o texto na foto
+〲┴┬➣ USO: Enviar imagem com legenda
+〲
+〲┴┬➣ COMANDO: '${prefix}wait'
+〲┴┬➣ DESC: Procure anime [Que anime é]
+〲┴┬➣ USO: Enviar imagem com legenda
+〲
+〲┴┬➣ COMANDO: '${prefix}clone'
+〲┴┬➣ DESC: Usar foto de perfil da pessoa
+〲┴┬➣ USO: .clone @member
+〲┴┬➣ NOTA: Usado por adms
+〲
+〲┴┬➣COMANDO:  '${prefix}db'
+〲┴┬➣DESC: Envia link com database de travazap
+〲┴┬➣USO: Apenas envie o comando 
+〲
+〲┴┬➣COMANDO:  '${prefix}wppim'
+〲┴┬➣DESC: Envia link de Whatsapp imunes
+〲┴┬➣USO: Apenas envie o comando 
+〲
+〲┴┬➣COMANDO:  '${prefix}nethunter'
+〲┴┬➣DESC: Tutorial de kali linux no seu termux
+〲┴┬➣USO: Apenas envie o comando 
+〲
+〲┴┬➣ COMANDO: '${prefix}listaadmins'
+〲┴┬➣ DESC: Marca todos os adms do grupo
+〲┴┬➣ USO: .listaadmins
+〲┴┬➣ NOTA: Usado por membros do grupo
+〲
+〲┴┬➣ COMANDO: '${prefix}nulis'
+〲┴┬➣ DESC: Foto com seu texto no caderno
+〲┴┬➣ USO: .nulis [texto]
+〲┴┬➣ NOTA: Usado em grupos
+〲
+〲┴┬➣ COMANDO: '${prefix}signome'
+〲┴┬➣ DESC: Mostra o significado do seu nome
+〲┴┬➣ USO:  '${prefix}signome [nome]'
+〲
+〲┴┬➣ COMANDO: '${prefix}blocklist'
+〲┴┬➣ DESC Lista de ctts bloqueados
+〲┴┬➣ USO: .blocklist
+〲┴┬➣ NOTA: Usado em grupos
+〲
+〲︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶
+〲
+〲╭─────⊣〘 𝗖𝗢𝗠𝗔𝗡𝗗𝗢𝗦 +18 〙
+〲│
+〲┴┬➣COMANDO:  *${prefix}nekoanime*
+〲┴┬➣DESC: Imagens aleatórias de neko
+〲┴┬➣USO: Apenas envie o comando
+〲
+〲┴┬➣ COMANDO: '${prefix}loli'
+〲┴┬➣ DESC: Imagens de loli
+〲┴┬➣ USO: Apenas envie o comando
+〲
+〲┴┬➣COMANDO: '${prefix}randomanime-
+〲┴┬➣DESC: Imagens aleatórias de anime
+〲┴┬➣USO: Apenas envie o comando 
+〲
+〲┴┬➣COMANDO: '${prefix}randomhentai'
+〲┴┬➣DESC: Imagens aleatórias de hentai
+〲┴┬➣USO: Apenas envie o comando 
+〲
+〲┴┬➣COMANDO: '${prefix}nsfwblowjob'
+〲┴┬➣DESC: Gifs de blowsjob de hentai
+〲┴┬➣USO: Apenas envie o comando 
+〲
+〲┴┬➣COAMANDO:  '${prefix}nsfwneko'
+〲┴┬➣DESC: Imagens de hentai neko
+〲┴┬➣USO: Apenas envie o comando 
+〲
+〲┴┬➣COMANDO:  '${prefix}nsfwtrap'
+〲┴┬➣DESC: Imagens de hentai trap
+〲┴┬➣USO: Apenas envie o comando 
+〲
+〲┴┬➣COMANDO:  '${prefix}nsfwloli'
+〲┴┬➣DESC: Imagens de hentai
+〲┴┬➣USO: Apenas envie o comando 
+〲┴┬➣NOTA: Funciona somente no pv do bot, ou se for adm
+〲
+〲┴┬➣COMANDO:  '${prefix}shota'
+〲┴┬➣DESC: Imagens aleatórias de shota (loli masculinas)
+〲┴┬➣USO: Apenas envie o comando 
+〲
+〲┴┬➣COMANDO:  '${prefix}pack'
+〲┴┬➣DESC: Pacotes de links +18
+〲┴┬➣USO: Apenas envie o comando
+〲
+〲┴┬➣COMANDO:  '${prefix}nsfw'
+〲┴┬➣DESC: Ativar o modo hentai no grupo 
+〲┴┬➣USO:  '${prefix}nsfw' 1/0 ativar e desativar 
+〲┴┬➣OBS: Este comando só pode ser usado se você for adm
+〲
+〲︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶
+〲
+〲╭─────⊣〘 𝗚𝗥𝗨𝗣𝗢 〙
+〲│
+〲┴┬➣ COMANDO: '${prefix}add'
+〲┴┬➣ DESC: Adicionar membro ao grupo
+〲┴┬➣ USO: .add 55xxxxxxxx
+〲┴┬➣ NOTA: Usado quando o bot é admin!
+〲
+〲┴┬➣ COMANDO: '${prefix}kick'
+〲┴┬➣ DESC: Banir membros do grupo
+〲┴┬➣ USO: .kick @member
+〲┴┬➣ NOTA: Usado quando o bot é admin!
+〲
+〲┴┬➣ COMANDO: '${prefix}promote'
+〲┴┬➣ DESC: Torna um membro como adm 
+〲┴┬➣ USO: .promote @member
+〲┴┬➣ NOTA: Usado quando o bot é admin!
+〲
+〲┴┬➣ COMANDO: '${prefix}demote'
+〲┴┬➣ DESC: Tornar o um adm como membro
+〲┴┬➣ USO: .demote @member
+〲┴┬➣ NOTA: Usado quando o bot é admin!
+〲
+〲┴┬➣ COMANDO: '${prefix}linkgroup'
+〲┴┬➣ DESC: Enviar o link do grupo
+〲┴┬➣ USO: Apenas envie o comando
+〲┴┬➣ NOTA: Usado quando o bot é admin!
+〲
+〲┴┬➣ COMANDO: '${prefix}simih'
+〲┴┬➣ DESC: Ative o modo simi no grupo
+〲┴┬➣ ATIVAR: .simih 1 
+〲┴┬➣ DESATIVAR: .simih 0
+〲┴┬➣ NOTA: Usado quando você é adm
+〲
+〲┴┬➣ COMANDO: '${prefix}welcome'
+〲┴┬➣ DESC: Ative o recurso de boas vindas
+〲┴┬➣ ATIVAR: .welcome 1 
+〲┴┬➣ DESATIVAR: .welcome 0
+〲┴┬➣ NOTA: Usado quando o bot é adm!
+〲
+〲︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶
+〲
+〲╭─────⊣〘 𝗖𝗢𝗠𝗔𝗡𝗗𝗢𝗦 𝗗𝗘 𝗧𝗘𝗫𝗧𝗢 〙
+〲│
+〲┴┬➣COMANDO:  '${prefix}frase'
+〲┴┬➣DESC: Envia uma frase filosófica 
+〲┴┬➣USO: Apenas envie o comando 
+〲
+〲┴┬➣COMANDO:  '${prefix}textph'
+〲┴┬➣DESC: Converte texto na logo do pornhub
+〲┴┬➣USO:  '${prefix}textph [pt1] | [pt2]'
+〲
+〲┴┬➣COMANDO:  '${prefix}traduz'
+〲┴┬➣DESC: Traduz seu texto em outra língua
+〲┴┬➣USO:  '${prefix}traduz [pt] [texto]'
+〲
+〲┴┬➣COMANDO:  '${prefix}idioma'
+〲┴┬➣DESC: Lista todos os códigos de idiomas
+〲┴┬➣USO: Apenas envie o comando 
+〲
+〲┴┬➣COMANDO:  '${prefix}text3d'
+〲┴┬➣DESC: Converte o texto em imagem 3d
+〲┴┬➣USO:  '${prefix}text3d [pt1]'
+〲
+〲┴┬➣COMANDO:  '${prefix}wolflogo'
+〲┴┬➣DESC: Converte o texto em imagem com logo de um lobo
+〲┴┬➣USO:  '${prefix}wolflogo [pt1] | [pt2]'
+〲
+〲┴┬➣COMANDO:  '${prefix}lionlogo'
+〲┴┬➣DESC: Converte texto em imagem com logo de um leao
+〲┴┬➣USO:  '${prefix}lionlogo [pt1] | [pt2]'
+〲
+〲┴┬➣COMANDO:  '${prefix}glitch'
+〲┴┬➣DESC: Converte texto em imagem com estilo glitch
+〲┴┬➣USO:  '${prefix}glitch [pt1] | [pt2]'
+〲
+〲┴┬➣COMANDO:  '${prefix}ninjalogo'
+〲┴┬➣DESC: Converte texto em imagem com logo de um ninja
+〲┴┬➣USO:  '${prefix}ninjalogo [pt1] | [pt2]'
+〲
+〲┴┬➣COMANDO:  '${prefix}neonligth'
+〲┴┬➣DESC: Converte texto em imagem com estilo neon ligth
+〲┴┬➣USO:  '${prefix}neonligth [pt1] | [pt2]'
+〲
+〲┴┬➣COMANDO:  '${prefix}snow'
+〲┴┬➣DESC: Converter texto em imagem com estilo snow
+〲┴┬➣USO:  '${prefix}snow [pt1]'
+〲
+〲┴┬➣COMANDO:  '${prefix}blood'
+〲┴┬➣DESC: Converter texto em imagem com estilo blood
+〲┴┬➣USO  '${prefix}blood [pt1]'
+〲
+〲┴┬➣COMANDO:  '${prefix}neontec'
+〲┴┬➣DESC: Converter texto em imagem com estilo tecnology
+〲┴┬➣USO:  '${prefix}neontec [pt1]'
+〲
+〲┴┬➣COMANDO:  '${prefix}dropwater'
+〲┴┬➣DESC: Converter texto em imagem com estilo dropwater
+〲┴┬➣USO:  '${prefix}dropwater [pt1]'
+〲
+〲︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶
+〲
+〲╭────⊣〘 𝗘𝗫𝗧𝗥𝗔𝗦 〙
+〲│
+〲┴┬➣ COMANDO:  '${prefix}termux'
+〲┴┬➣ DESC: Mostra uma lista com comandos do termux
+〲┴┬➣ USO: Apenas envie o comando 
+〲
+〲︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶
+〲╭────⊣〘 𝗦𝗨𝗣𝗢𝗥𝗧𝗘 〙
+〲│
+〲┴┬➣ Wpp: wa.me/5596984081026
+〲︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶
+〲 
+〲 ║▌│█║▌│ █║▌│█│║▌║
+〲 ║▌│█║▌│ █║▌│█│║▌║
+〲
+〲 Copyright © 𝐆𝐀𝐀𝐑𝐀𝐙𝐈𝐍𝐇𝐎 愛 2021
+〲
+︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶
+`
+}
+
+exports.help = help
+
+
+
+
---
+++ src/wppimune.js
@@ -0,0 +1,37 @@
+const databases = (prefix) => {
+
+return `
+╔══✪〘 𝗗𝗔𝗧𝗔𝗕𝗔𝗦𝗘 𝗧𝗥𝗔𝗩𝗔𝗦 〙✪══
+║
+╠➥ DB NESCAU V8 DIAMOND: https://enrt.eu/0o2tqQ
+╠➥ 𝐃𝐁 𝐕5 𝐁𝐘 𝐆𝐑𝐈𝐍𝐆𝐎 𝐌𝐃𝐙🔥: https://enrt.eu/8aZDGC
+╠➥ DATABASE ELMØ V10: https://enrt.eu/KqBGh
+╠➥ DATABASE DO DAVIZINMAKER: https://enrt.eu/e8AaU1w
+║
+╚════════════════════
+`
+}
+
+exports.databases = databases
+
+const imune = (prefix) => {
+return `
+
+╔══✪〘 𝗪𝗛𝗔𝗧𝗦𝗔𝗣𝗣 𝗜𝗠𝗨𝗡𝗘𝗦 〙✪══
+║
+╠➥ GAARA v5: https://suaurl.com/dde464
+╠➥ MINATO v8: https://suaurl.com/e57887
+╠➥ TERREMOTO v22: https://suaurl.com/210d49
+╠➥ IMORTAL v19: https://suaurl.com/225875
+╠➥ Souza Wa v5: https://suaurl.com/8dbb5e
+╠➥ DIURNO v9: http://www.mrtutors.com.br/IDKFr
+╠➥ FROST v17: https://suaurl.com/2eb5cc
+╠➥ FÊNIX v14: https://suaurl.com/145ff5
+╠➥ PESADELO v7: https://suaurl.com/ddf8a6
+╠➥ FLEX v7: https://suaurl.com/b18e0e
+║
+╚═══════════════════
+`
+}
+
+exports.imune = imune
